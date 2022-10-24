9aa2efaa714b446310a7abd9387c2cf48a5dc2e8
BAMobile <78639992+BAMobile@users.noreply.github.com>
2022-10-24 16:40:15 +0300

    Update Form2.cs

--- Form2.cs
+++ Form2.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WindowsFormsApp2
@@ -22,17 +15,18 @@ namespace WindowsFormsApp2
             try
             {
                 Book book = new Book(Book.maxVal + 1, textBox1.Text, textBox2.Text, Int32.Parse(textBox3.Text), false);
-                Form1.form1instance.books.Add(book);
-                Form1.form1instance.bindingSource1.ResetBindings(false);
+               Form1.form1instance.bindingSource1.Add(book)
             }
             catch (Exception ex)
             {
+                // TODO - якщо некорректне число, то не маємо закривати вікно і дати можливість користувачу виправити
                 MessageBox.Show("Число введено невірно","Помилка!", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
             
             this.Close();
         }
 
+        //TODO - видаляємо, оскільки відсутня обробка
         private void bindingSource1_CurrentChanged(object sender, EventArgs e)
         {
 
